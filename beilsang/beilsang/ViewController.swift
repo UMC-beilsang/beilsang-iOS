ec97ec1dc5a0d0efc1f79b4eaf6b3a4bc69f0441
Seyoung <seyoung@30Mackbookair.local>
2024-01-16 13:53:09 +0900

    #1 feat: first commit

---
+++ beilsang/beilsang/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  beilsang
+//
+//  Created by Seyoung on 1/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let test = UILabel()
+        view.backgroundColor = .white // 배경색
+        view.addSubview(test)
+        test.text = "text" // test를 위해서 출력할 라벨
+        test.translatesAutoresizingMaskIntoConstraints = false
+        test.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        test.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
