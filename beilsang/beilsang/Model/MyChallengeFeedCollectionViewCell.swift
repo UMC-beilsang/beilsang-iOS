d26fbd48066077710f1479fad3a5bdb0091d39cf
633jinn <judyg00dy68@gmail.com>
2024-01-22 23:30:38 +0900

    #2 feat: 마이페이지 메인 화면 View 구현

--- beilsang/beilsang/Model/MyChallengeFeedCollectionViewCell.swift
+++ beilsang/beilsang/Model/MyChallengeFeedCollectionViewCell.swift
@@ -14,9 +14,29 @@ class MyChallengeFeedCollectionViewCell: UICollectionViewCell{
         let feedButton = UIButton()
         feedButton.setImage(UIImage(named: "Mask group"), for: .normal)
         feedButton.layer.borderWidth = 1
+        feedButton.layer.borderColor = UIColor(red: 0.902, green: 0.902, blue: 0.902, alpha: 1).cgColor
         feedButton.layer.cornerRadius = 10
-        feedButton.backgroundColor = .red
+        feedButton.backgroundColor = .white
         return feedButton
     }()
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        setupAttribute()
+        setConstraint()
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupAttribute() {
+        self.addSubview(challengeFeed)
+    }
+    private func setConstraint() {
+        challengeFeed.translatesAutoresizingMaskIntoConstraints = false
+        challengeFeed.snp.makeConstraints({ make in
+            make.width.equalTo(173)
+            make.height.equalTo(140)
+        })
+    }
 }
