17d06c027da6b3339e276dd232c1133cc297c014
633jinn <judyg00dy68@gmail.com>
2024-01-24 10:57:54 +0900

    #2 feat: .gitignore 추가, 기존에 만든 파일 추가

---
+++ beilsang/beilsang/View/MyPage/MyChallengeFeedCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  MyChallengeFeedCollectionView.swift
+//  beilsang
+//
+//  Created by 강희진 on 1/22/24.
+//
+
+import UIKit
+class MyChallengeFeedCollectionViewCell: UICollectionViewCell{
+    
+    static let identifier = "MyChallengeFeedVC"
+    
+    lazy var challengeFeed: UIButton = {
+        let feedButton = UIButton()
+        feedButton.setImage(UIImage(named: "Mask group"), for: .normal)
+        feedButton.layer.borderWidth = 1
+        feedButton.layer.borderColor = UIColor(named: "challengeLine")?.cgColor
+        feedButton.layer.cornerRadius = 10
+        feedButton.backgroundColor = .white
+        return feedButton
+    }()
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        setupAttribute()
+        setConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupAttribute() {
+        self.addSubview(challengeFeed)
+    }
+    private func setConstraint() {
+        challengeFeed.translatesAutoresizingMaskIntoConstraints = false
+        challengeFeed.snp.makeConstraints({ make in
+            make.width.equalTo(173)
+            make.height.equalTo(140)
+        })
+    }
+}
