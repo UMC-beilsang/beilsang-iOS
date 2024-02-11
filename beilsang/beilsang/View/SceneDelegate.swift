c96eac10ae2e2af29113122d5e8b3f1eaf9a8f46
Seyoung <seyoung@30Mackbookair.local>
2024-02-11 18:48:56 +0900

    hotfix : git ignore 편집, scene delegate 추가

--- beilsang/beilsang/View/SceneDelegate.swift
+++ beilsang/beilsang/View/SceneDelegate.swift
@@ -14,7 +14,7 @@ import KakaoSDKUser
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
-    
+    //로그인 로직
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
