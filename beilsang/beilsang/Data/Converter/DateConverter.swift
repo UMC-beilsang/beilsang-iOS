c9532ee63cfc293c42a03924300599c341ac36b3
kwaktato <happyrhkr@g.hongik.ac.kr>
2024-02-16 01:38:49 +0900

    #4 feat: 카테고리, 날짜, 실천기간 변환 class 생성

---
+++ beilsang/beilsang/Data/Converter/DateConverter.swift
@@ -0,0 +1,67 @@
+//
+//  DateConverter.swift
+//  beilsang
+//
+//  Created by 곽은채 on 2/16/24.
+//
+
+// 날짜 형태를 변환하는 class
+// 서버 yyyy-MM-dd와 프론트 yyyy년 MM월 dd일 형태를 서로 변환하기 위함
+import Foundation
+
+class DateConverter {
+    static let shared = DateConverter()
+    
+    private let serverFormatter: DateFormatter
+    private let frontFormatter: DateFormatter
+    
+    private let detailFormatter: DateFormatter
+    private let dayMap = [
+        "MONDAY": "(월)",
+        "TUESDAY": "(화)",
+        "WEDNESDAY": "(수)",
+        "THURSDAY": "(목)",
+        "FRIDAY": "(금)",
+        "SATURDAY": "(토)",
+        "SUNDAY": "(일)"
+    ]
+    
+    private init() {
+        serverFormatter = DateFormatter()
+        serverFormatter.dateFormat = "yyyy-MM-dd" // 서버 날짜 형식
+        
+        frontFormatter = DateFormatter()
+        frontFormatter.dateFormat = "yyyy년 MM월 dd일" // 화면 표시 날짜 형식
+        
+        detailFormatter = DateFormatter()
+        detailFormatter.dateFormat = "MM. dd" // ChallengeDetailVC 날짜 형식
+    }
+    
+    func convertToFrontFormat(from serverDate: String) -> String? {
+        if let date = serverFormatter.date(from: serverDate) {
+            return frontFormatter.string(from: date)
+        }
+        
+        return nil
+    }
+    
+    func convertToServerFormat(from displayDate: String) -> String? {
+        if let date = frontFormatter.date(from: displayDate) {
+            return serverFormatter.string(from: date)
+        }
+        
+        return nil
+    }
+    
+    func convertToDay(_ day: String) -> String? {
+        return dayMap[day]
+    }
+    
+    func convertDetail(from serverDate: String) -> String? {
+        if let date = serverFormatter.date(from: serverDate) {
+            return detailFormatter.string(from: date)
+        }
+        
+        return nil
+    }
+}
---
+++ beilsang/beilsang/Data/Converter/PeriodConverter.swift
@@ -0,0 +1,27 @@
+//
+//  PeriodConverter.swift
+//  beilsang
+//
+//  Created by 곽은채 on 2/16/24.
+//
+
+// 실천 기간 형태 변환을 위한 class
+// 각 코드에 써도 되지만 코드가 너무 길어져서 만드는 김에 생성
+import Foundation
+
+class PeriodConverter {
+    static let shared = PeriodConverter()
+
+    private let periodMap = ["일주일": "WEEK", "한달": "MONTH"]
+    private let periodEngMap = ["WEEK": "일주일", "MONTH": "한달"]
+
+    private init() {}
+
+    func convertToEnglish(_ period: String) -> String? {
+        return periodMap[period]
+    }
+
+    func convertToKorean(_ period: String) -> String? {
+        return periodEngMap[period]
+    }
+}
